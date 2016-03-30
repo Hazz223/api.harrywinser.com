efe0ba8bfdb252d2e895e0dcb51e2d2dc0551fb7
Harry Winser <harrywinser@hotmail.co.uk>
2016-03-30 10:53:48 +0100

    Added gradle and other cool things. this should be the framework for everything.

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,18 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class Application {
+
+    //region Public Methods
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    //endregion
+
+}
