5d6c594b896d5588800abfbc561785401067de5d
Harry Winser <harrywinser@hotmail.co.uk>
2016-10-01 20:49:26 +0100

    Use the new builder in the converter

--- src/main/java/com/harry/winser/api/services/converters/ArticleToDtoConverter.java
+++ src/main/java/com/harry/winser/api/services/converters/ArticleToDtoConverter.java
@@ -2,6 +2,7 @@ package com.harry.winser.api.services.converters;
 
 import com.harry.winser.api.domain.article.Article;
 import com.harry.winser.api.web.dto.ArticleDto;
+import com.harry.winser.api.web.dto.ArticleDtoBuilder;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +12,13 @@ public class ArticleToDtoConverter implements Converter<Article, ArticleDto> {
     @Override
     public ArticleDto convert(Article source) {
 
-        ArticleDto articleDto = new ArticleDto();
-
-        articleDto.setId(source.getId());
-        articleDto.setCleanTitle(source.getCleanTitle());
-        articleDto.setTitle(source.getTitle());
-        articleDto.setType(source.getType().toString().toLowerCase());
-        articleDto.setData(source.getData());
-        articleDto.setDate(source.getCreateDate());
-
-        return articleDto;
+        return ArticleDtoBuilder.anArticleDto()
+                .withId(source.getId())
+                .withCleanTitle(source.getCleanTitle())
+                .withTitle(source.getTitle())
+                .withType(source.getType())
+                .withData(source.getData())
+                .withDate(source.getCreateDate())
+                .build();
     }
 }
