cdc4e73dd5776efcde717ce3470b235e621c293b
Harry Winser <harrywinser@hotmail.co.uk>
2017-04-11 20:33:27 +0100

    Add Docker Secrets code so it can be loaded as a docker service

--- src/main/java/com/harry/winser/api/Application.java
+++ src/main/java/com/harry/winser/api/Application.java
@@ -1,18 +1,27 @@
 package com.harry.winser.api;
 
+import com.harry.winser.docker.secrets.DockerSecretsException;
+import com.harry.winser.docker.secrets.DockerSecretsLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableWebMvc
 public class Application {
 
-    //region Public Methods
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    //endregion
+
+    @PostConstruct
+    public void loadDockerSecrets() throws DockerSecretsException {
+
+        Map<String, String> secrets = new DockerSecretsLoader().loadAsMap();
+        secrets.forEach(System::setProperty);
+    }
 
 }
