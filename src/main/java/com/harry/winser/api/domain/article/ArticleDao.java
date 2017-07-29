b517e4b0079f23535dc8c5e52bdc2e9c7c105411
Harry Winser <harrywinser@hotmail.co.uk>
2017-07-29 16:16:55 +0100

    Remove pointless import

--- src/main/java/com/harry/winser/api/domain/article/ArticleDao.java
+++ src/main/java/com/harry/winser/api/domain/article/ArticleDao.java
@@ -3,7 +3,6 @@ package com.harry.winser.api.domain.article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
