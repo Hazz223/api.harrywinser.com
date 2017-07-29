a9fb64084266407aee153f5249f988fe8b8aa488
Harry Winser <harrywinser@hotmail.co.uk>
2017-07-29 11:03:25 +0100

    Add HikariCP to handle sessions

--- src/main/java/com/harry/winser/api/Application.java
+++ src/main/java/com/harry/winser/api/Application.java
@@ -1,13 +1,9 @@
 package com.harry.winser.api;
 
-import com.harry.winser.docker.secrets.DockerSecretsException;
-import com.harry.winser.docker.secrets.DockerSecretsLoader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.annotation.PostConstruct;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableWebMvc
